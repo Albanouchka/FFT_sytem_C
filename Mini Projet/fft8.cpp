cfedaccb6b734e2e0a0d60036b9b5c0ffac4ef1a
Albanouchka <46610889+Albanouchka@users.noreply.github.com>
2022-02-22 14:33:15 +0100

    Add files via upload

--- Mini Projet/fft8.cpp
+++ Mini Projet/fft8.cpp
@@ -1,13 +1,86 @@
+/*
+ * Author: Pierre-Henri Horrein
+ * Pure software C implementation of a 8 point FFT based on Cooley-Tuckey algorithm
+ * This is a floating point implementation
+ */
+
+#include <stdio.h>
 #include "fft8.h"
 
-#include <fstream>
-#include <iostream>
+
+complex_t weights[4] = W;
+complex_t in[8];
+complex_t out[8];
+
+int compteur =0;
+float tmp[16];
+
+
+void but(complex_t *weight,
+					complex_t *in0,
+					complex_t *in1,
+					complex_t *out0,
+					complex_t *out1)
+{
+      out0->real = (in0->real + ((in1->real * weight->real) - (in1->imag * weight->imag)));
+      out0->imag = (in0->imag + ((in1->real * weight->imag) + (in1->imag * weight->real)));
+      out1->real = (in0->real - ((in1->real * weight->real) - (in1->imag * weight->imag)));
+      out1->imag = (in0->imag - ((in1->real * weight->imag) + (in1->imag * weight->real)));
+}
+
+	
+void fft(complex_t in[8], complex_t out[8]) 
+{
+	complex_t stage1[8], stage2[8];
+
+	// First stage
+	but(&weights[0], &in[0], &in[4], &stage1[0], &stage1[1]);
+	but(&weights[0], &in[2], &in[6], &stage1[2], &stage1[3]);
+	but(&weights[0], &in[1], &in[5], &stage1[4], &stage1[5]);
+	but(&weights[0], &in[3], &in[7], &stage1[6], &stage1[7]);
+
+	// Second stage
+	but(&weights[0], &stage1[0], &stage1[2], &stage2[0], &stage2[2]);
+	but(&weights[2], &stage1[1], &stage1[3], &stage2[1], &stage2[3]);
+	but(&weights[0], &stage1[4], &stage1[6], &stage2[4], &stage2[6]);
+	but(&weights[2], &stage1[5], &stage1[7], &stage2[5], &stage2[7]);
+                
+	// Etape 3
+	but(&weights[0], &stage2[0], &stage2[4], &out[0], &out[4]);
+	but(&weights[1], &stage2[1], &stage2[5], &out[1], &out[5]);
+	but(&weights[2], &stage2[2], &stage2[6], &out[2], &out[6]);
+	but(&weights[3], &stage2[3], &stage2[7], &out[3], &out[7]);
+}
+
 
 
 void FFT8::COMPORTEMENT(){
 
 while(true){
-    fft_out.write(fft_in.read());
-    wait();
-}
+
+
+    if(compteur < 16){
+        tmp[compteur] = fft_in.read();
+        compteur++;
+    }
+    else{
+        for(int j = 0; j < 8; j++){
+            in[j].real = tmp[2*j];
+            in[j].imag = tmp[2*j+1];
+        }
+        
+        fft(in, out);
+        compteur = 0;
+        
+        for(int k = 0; k<8; k++){
+            fft_out.write(out[k].real);
+            fft_out.write(out[k].imag);
+        } 
+    }
+    wait();      
+
+};
+
+
+
 }
--- Mini Projet/fft8.h
+++ Mini Projet/fft8.h
@@ -3,6 +3,22 @@
 #ifndef FFT8_H
 #define FFT8_H
 
+#define W {\
+	{1, 0}, \
+	{0.7071067812, -0.7071067812}, \
+	{0, -1}, \
+	{-0.7071067812, -0.7071067812}\
+}
+
+typedef struct complex_s complex_t;
+
+struct complex_s {
+	double real;
+	double imag;
+};
+
+void fft(complex_t in[8], complex_t out[8]);
+
 SC_MODULE(FFT8){
     
     public :
